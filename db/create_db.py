d66e17189833fed9743c50a843fb048f74b1ff22
Maddie Wagner <musomaddy@gmail.com>
2020-06-14 13:16:59 +1000

    DB now storing unicorn details! :)

--- db/create_db.py
+++ db/create_db.py
@@ -1,6 +1,53 @@
 import sqlite3
+import csv
 
 DB_NAME = "UnstableUnicorns.db"
+UNICORN_FILE_NAME = "UnicornDetails.tsv"
+
+
+def _load_all_unicorns():
+    unicorns = []
+    with open(UNICORN_FILE_NAME, newline='') as f:
+        reader = csv.DictReader(f, dialect='excel-tab')
+        for row in reader:
+            unicorns.append(row)
+    return unicorns
+
+
+def populate_db():
+    conn = sqlite3.connect(DB_NAME)
+    cur = conn.cursor()
+    unicorns = _load_all_unicorns()
+    for unicorn in unicorns:
+        # Perform string replacement of double quotes
+        for key in unicorn:
+            unicorn[key] = unicorn[key].replace('"', "'")
+        cur.execute(f'''
+                    INSERT INTO unicorn_details
+                    VALUES (
+                        {unicorn['Counter']},
+                        "{unicorn["Card Name"]}",
+                        "{unicorn["Type"]}",
+                        "{unicorn["Text"]}",
+                        "{unicorn["Action on Enter Stable"]}",
+                        "{unicorn["Beginning of Turn Action"]}",
+                        "{unicorn["Discard Action"]}",
+                        "{unicorn["In Stable Effect"]}",
+                        "{unicorn["Search Deck"]}",
+                        "{unicorn["Shuffle Discard Into Deck"]}",
+                        "{unicorn["Scarifice"]}",
+                        "{unicorn["Return to Hand"]}",
+                        "{unicorn["Search Discard"]}",
+                        "{unicorn["Protection"]}",
+                        "{unicorn["Draw"]}",
+                        "{unicorn["Destroy"]}",
+                        "{unicorn["Requires Basic Unicorn"]}",
+                        "{unicorn["Action on Leave"]}"
+                    );
+                    ''')
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 def init_db():
@@ -16,12 +63,12 @@ def init_db():
                     card_type TEXT,
                     action_on_enter BOOLEAN,
                     action_on_begin BOOLEAN,
-                    discard_action BOOLEAN,
+                    discard_action TEXT,
                     in_stable_effect BOOLEAN,
                     search_deck TEXT,
                     shuffle_deck BOOLEAN,
                     shuffle_discard BOOLEAN,
-                    scarifice TEXT,
+                    sacrifice TEXT,
                     return_to_hand BOOLEAN,
                     search_discard TEXT,
                     protection TEXT,
@@ -49,4 +96,4 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    # populate_db()
+    populate_db()

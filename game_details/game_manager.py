de1eedb395fa829c4f4cff83c3ebf17830e385f5
Maddie Wagner <musomaddy@gmail.com>
2020-06-15 19:51:27 +1000

    Now creating the correct number of cards based on classified starting decks and stored quantity.
    The database needs to be revised: the card quantities seem wrong

--- game_details/game_manager.py
+++ game_details/game_manager.py
@@ -11,18 +11,35 @@ from game_details.Card import Card
 DB_NAME = "db/UnstableUnicorns.db"
 DECK = []
 DISCARD_PILE = []
+PLAYERS = []
 
 
-def create_game():
-    # TODO: choose number players
-    # TODO: choose the decks to play with
+def create_game(starting_decks):
+    DECK.clear()
+    DISCARD_PILE.clear()
+    PLAYERS.clear()
+    valid_decks = ["Standard", "Rainbow", "Dragon", "Uncut", "NSFW"]
+
+    if len(starting_decks) == 0:
+        return
+    if len(starting_decks) == 1 and starting_decks[0] != "Standard":
+        # TODO: maybe edge case where all nonsense decks but one which isn't
+        # standard
+        return
 
     conn = sqlite3.connect(DB_NAME)
     cur = conn.cursor()
 
-    cur.execute("SELECT * FROM card_details JOIN pack_details USING (name);")
-    [DECK.append(Card(result)) for result in cur.fetchall()
-     for _ in range(0, result[-2])]
+    for deck in starting_decks:
+        if deck not in valid_decks:
+            continue
+        cur.execute(f"""SELECT *
+                        FROM card_details JOIN pack_details USING (name)
+                        WHERE deck='{deck}';
+                    """)
+        [DECK.append(Card(result)) for result in cur.fetchall()
+            for _ in range(0, result[-2])]
+
     random.shuffle(DECK)
 
     cur.close()
@@ -30,5 +47,4 @@ def create_game():
 
 
 if __name__ == '__main__':
-    create_game()
-    print(DECK)
+    create_game(["Standard", "Dragon"])
--- tests/game_details/game_manager_tests.py
+++ tests/game_details/game_manager_tests.py
@@ -1,10 +1,55 @@
 import unittest
+import os
+import sys
+import sqlite3
+
+sys.path.insert(0,
+                os.path.dirname(os.path.realpath(__file__))[
+                    0:-len("tests/game_details")])
+
+from game_details.game_manager import create_game
+from game_details.game_manager import DECK as deck
+
+NUM_CARDS_STANDARD = 111
+NUM_CARDS_RAINBOW = 44
+NUM_CARDS_NSFW = 42
+NUM_CARDS_UNCUT = 41
+NUM_CARDS_DRAGON = 49
 
 
-class TestCass(unittest.TestCase):
+class StartingDeck(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+    def test_all_decks(self):
+        create_game(["Standard", "Dragon", "Rainbow", "Uncut", "NSFW"])
+        self.assertEqual(len(deck),
+                         NUM_CARDS_STANDARD + NUM_CARDS_RAINBOW
+                         + NUM_CARDS_NSFW + NUM_CARDS_UNCUT
+                         + NUM_CARDS_DRAGON)
+
+    def test_nsfw_decks(self):
+        create_game(["Uncut", "NSFW"])
+        self.assertEqual(len(deck),
+                         NUM_CARDS_NSFW + NUM_CARDS_UNCUT)
+
+    def test_one_deck_not_standard(self):
+        create_game(["Dragon"])
+        self.assertEqual(len(deck), 0)
+
+    def test_only_standard(self):
+        create_game(["Standard"])
+        self.assertEqual(len(deck), NUM_CARDS_STANDARD)
+
+    def test_nonexistent_deck(self):
+        create_game(["FOO"])
+        self.assertEqual(len(deck), 0)
+
+    def test_no_deck(self):
+        create_game([])
+        self.assertEqual(len(deck), 0)
+
+    def test_nonexistent_deck_with_existent(self):
+        create_game(["FOO", "Standard"])
+        self.assertEqual(len(deck), NUM_CARDS_STANDARD)
 
 
 if __name__ == '__main__':

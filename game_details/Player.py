46970e576c722cdc2b7a16f298ee9b7fe92f1797
Maddie Wagner <musomaddy@gmail.com>
2022-03-13 18:57:33 +1100

    Adding Hand and Stable Manager so the player is hopefully all connected up.

--- game_details/Player.py
+++ game_details/Player.py
@@ -1,3 +1,15 @@
+import os
+import sys
+
+sys.path.insert(0, 
+        os.path.dirname(os.path.realpath(__file__))[
+            0:-len("game_details")])
+
+
+from game_details.HandManager import HandManager
+from game_details.StableManager import StableManager
+
+
 class Player:
     """ Represents a player in the game.
 
@@ -9,8 +21,8 @@ class Player:
 
     def __init__(self, name):
         self.name = name
-        self.hand = [] # TODO: update when HandManager is created.
-        self.stable = [] # TODO: update when StableManager is created.
+        self.hand = HandManager(player=self)
+        self.stable = StableManager(player=self)
 
     def __repr__(self):
         return f"{self.name} (player)"

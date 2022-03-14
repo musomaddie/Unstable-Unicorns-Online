31b8374290f7adfaf0d13491c3aa6d8d72f64a56
Maddie Wagner <musomaddy@gmail.com>
2022-03-14 15:35:16 +1100

    Removing the default arguments as they resulted in all the hand and stables referencing the same place.

--- game_details/HandManager.py
+++ game_details/HandManager.py
@@ -5,12 +5,22 @@ class HandManager:
     Paramaters:
         player (player): the player whose hand this is.
         cards (List<Card>): all the cards currently in the hand.
+
+    Methods:
+        add_card(card): adds the given card to the hand.
     """
 
-    def __init__(self, player=None, cards=[]):
+    def __init__(self, player):
         self.player = player
-        self.cards = cards
+        self.cards = []
 
     def __repr__(self):
-        return f"The hand manager for {self.player.name} containing: " \
-               f"{' '.join(self.cards)}"
+        return f"Hand manager for {self.player.name}"
+
+    def add_card(self, card):
+        """ Adds the given card to the end of the hand.
+
+        Parameters:
+            card (Card): the card to add.
+        """
+        self.cards.append(card)
--- game_details/StableManager.py
+++ game_details/StableManager.py
@@ -1,3 +1,13 @@
+import os
+import sys
+
+sys.path.insert(0, 
+        os.path.dirname(os.path.realpath(__file__))[
+            0:-len("game_details")])
+
+from game_details.Card import CardType
+
+
 class StableManager:
     """ Represents a players stable. Responsible for managing all interactions
     with it.
@@ -7,10 +17,30 @@ class StableManager:
         unicorns (list<Card>): the unicorns currently in the stable
         upgrades (list<Card>): the upgrades currently in the stable
         downgrades (list<Card>): the downgrades currently in the stableA
+
+    Methods:
+        add_card(card): adds the given card to the stable.
     """
 
-    def __init__(self, player=None, unicorns=[], upgrades=[], downgrades=[]):
+    def __init__(self, player):
         self.player = player
-        self.unicorns = unicorns
-        self.upgrades = upgrades
-        self.downgrades = downgrades
+        self.unicorns = []
+        self.upgrades = []
+        self.downgrades = []
+
+    def __repr__(self):
+        return f"Stable Manager contains {len(self.unicorns)} unicorns for {self.player}"
+
+    def add_card(self, card):
+        """ Adds the given card to the stable.
+
+        Parameters:
+            card (Card): the card to add.
+        """
+        print(f"SM adding {card} for {self.player}")
+        if (card.card_type == CardType.UPGRADE):
+            self.upgrades.append(card)
+        elif (card.card_type == CardType.DOWNGRADE):
+            self.downgrades.append(card)
+        else:
+            self.unicorns.append(card)

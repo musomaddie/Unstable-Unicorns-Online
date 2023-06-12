50a327b489dadb0126332b79dd875f6180988085
Madeleine Wagner <musomaddy@gmail.com>
2023-06-13 08:49:09 +1000

    Actually using the argument in the helper method.

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,5 +14,5 @@ def create_card_stack_with_special_first_card(first_card: Card, other_card: Card
     card_pile = CardPile()
     card_pile.cards.append(first_card)
     for _ in range(10):
-        card_pile.cards.append(copy.copy(fake_card))
+        card_pile.cards.append(copy.copy(other_card))
     return card_pile

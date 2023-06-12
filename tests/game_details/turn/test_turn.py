1bd7e934253ae0a85668690e94a5fd768b3cce6d
Madeleine Wagner <musomaddy@gmail.com>
2023-06-12 13:50:00 +1000

    Updated turn test to now use a fixture.

--- tests/game_details/turn/test_turn.py
+++ tests/game_details/turn/test_turn.py
@@ -7,7 +7,7 @@ from game_details.deck import Deck
 from game_details.discard_pile import DiscardPile
 from game_details.nursery import Nursery
 from game_details.player import Player, AllPlayers
-from game_details.turn.turn import Turn
+from game_details.turn import Turn
 
 
 @pytest.fixture
@@ -15,13 +15,26 @@ def fake_card():
     return Card("Name", CardType.BASIC_UNICORN, "Text")
 
 
-def test_create_turn(fake_card):
-    players = [Player("Alice"), Player("Bob"), Player("Charlie")]
-    example_turn = Turn(
-        Deck([copy.copy(fake_card)]),
-        DiscardPile([copy.copy(fake_card)]),
-        Nursery([copy.copy(fake_card)]),
-        players[0],
-        AllPlayers(players)
-    )
-    print(example_turn)
+class TestTurnParts:
+    @pytest.fixture
+    def deck(self) -> Deck:
+        return Deck([copy.copy(fake_card) for _ in range(5)])
+
+    @pytest.fixture
+    def discard_pile(self) -> DiscardPile:
+        return DiscardPile()
+
+    @pytest.fixture
+    def nursery(self) -> Nursery:
+        return Nursery()
+
+    @pytest.fixture
+    def players(self) -> list[Player]:
+        return [Player("Alice"), Player("Bob"), Player("Charlie")]
+
+    @pytest.fixture
+    def turn(self, deck, discard_pile, nursery, players):
+        return Turn(deck, discard_pile, nursery, players[0], AllPlayers(players))
+
+    def test_take_turn(self, turn):
+        turn.take_turn()

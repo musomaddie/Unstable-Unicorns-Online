ca58f3c0e36604f7f279ff88f79e1e7904b0dbab
Madeleine Wagner <musomaddy@gmail.com>
2023-06-12 10:36:23 +1000

    Added discard pile manager file.

---
+++ game_details/discard_pile_manager.py
@@ -0,0 +1,10 @@
+from game_details.card import Card
+from game_details.card_pile_manager import CardPileManager
+
+
+class DiscardPileManager(CardPileManager):
+    """ A manager for the discard pile."""
+
+    def __init__(self, cards=list[Card]):
+        super().__init__(cards)
+        

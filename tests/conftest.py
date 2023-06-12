2bbd358195b44152fb0705741da01425dbc742ad
Madeleine Wagner <musomaddy@gmail.com>
2023-06-13 08:47:03 +1000

    Made a helper function in conftest to create a card stack with a special first card (remaining cards are copies of the second argument).

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,18 @@
+import copy
+
 import pytest
 
-from game_details.card import Card, CardType
+from game_details.card import Card, CardType, CardPile
 
 
 @pytest.fixture
 def fake_card():
     return Card("Name", CardType.BASIC_UNICORN, "Text")
+
+
+def create_card_stack_with_special_first_card(first_card: Card, other_card: Card) -> CardPile:
+    card_pile = CardPile()
+    card_pile.cards.append(first_card)
+    for _ in range(10):
+        card_pile.cards.append(copy.copy(fake_card))
+    return card_pile
--- tests/game_details/card/test_card_pile.py
+++ tests/game_details/card/test_card_pile.py
@@ -1,17 +1,13 @@
-import copy
-
-from game_details.card import CardPile, Card, CardType
+from game_details.card import Card, CardType
+from tests.conftest import create_card_stack_with_special_first_card
 
 
 def test_draw_card(fake_card):
-    card_pile = CardPile()
     special_card = Card("Special", CardType.BASIC_UNICORN, "Text")
-    card_pile.cards.append(special_card)
-    for _ in range(10):
-        card_pile.cards.append(copy.copy(fake_card))
+    card_pile = create_card_stack_with_special_first_card(special_card, fake_card)
     size_before = len(card_pile)
 
     result_card = card_pile.draw_top()
 
-    assert result_card.name == special_card.name
+    assert result_card == special_card
     assert len(card_pile) == size_before - 1

e7c2e83ec60d012881e26ca9ffee064808ce1950
Madeleine Wagner <musomaddy@gmail.com>
2023-06-13 08:46:25 +1000

    Added iterator to hand.

--- game_details/hand/hand.py
+++ game_details/hand/hand.py
@@ -10,5 +10,22 @@ class Hand:
     def __len__(self):
         return len(self.cards)
 
+    def __iter__(self):
+        return HandIterator(self)
+
     def add_card(self, card: Card) -> None:
         self.cards.append(card)
+
+
+class HandIterator:
+    """ An iterator for the hand, goes through and returns each card."""
+
+    def __init__(self, hand: Hand):
+        self._hand = hand
+        self._index = 0
+
+    def __next__(self):
+        if self._index < len(self._hand):
+            self._index += 1
+            return self._hand.cards[self._index - 1]
+        raise StopIteration

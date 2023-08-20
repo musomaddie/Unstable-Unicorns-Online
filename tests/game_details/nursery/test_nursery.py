6eb89530078d11687927479f5b834d5088a0be44
Maddie Wagner <musomaddy@gmail.com>
2023-08-20 18:41:34 +1000

    Added tests for the nuresry

---
+++ tests/game_details/nursery/test_nursery.py
@@ -0,0 +1,20 @@
+import pytest
+
+from game_details.card import CardType
+from game_details.nursery import Nursery
+
+
+@pytest.fixture
+def nursery():
+    return Nursery()
+
+
+def test_init(nursery):
+    assert len(nursery) == 25
+
+
+def test_get_baby(nursery):
+    size_before = len(nursery)
+    baby = nursery.get_baby()
+    assert baby.card_type == CardType.BABY_UNICORN
+    assert len(nursery) == size_before - 1

ccc98fc24d334b71faac85c4aa66e8bd52d8699c
Maddie Wagner <musomaddy@gmail.com>
2023-10-10 09:22:23 +1100

    Added iterator, len and getitem method to all_players.

--- game_details/player/all_players.py
+++ game_details/player/all_players.py
@@ -8,3 +8,26 @@ class AllPlayers:
     """ Manages all the players within the game."""
 
     players: list[Player]
+
+    def __len__(self):
+        return len(self.players)
+
+    def __getitem__(self, item):
+        return self.players[item]
+
+    def __iter__(self):
+        return AllPlayersIterator(self)
+
+
+class AllPlayersIterator:
+    """ An interater for the all players object. """
+
+    def __init__(self, all_players: AllPlayers):
+        self._all_players = all_players
+        self._index = 0
+
+    def __next__(self):
+        if self._index < len(self._all_players):
+            self._index += 1
+            return self._all_players[self._index - 1]
+        raise StopIteration
---
+++ tests/game_details/player/test_all_players.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock
+
+from _pytest.fixtures import fixture
+
+from game_details.player import Player, AllPlayers
+
+
+@fixture
+def all_players():
+    """ Creates an AllPlayers object containing 3 players - Alice, Bob and Charlie."""
+    return AllPlayers(
+        [Player(name, MagicMock(), MagicMock()) for name in ["Alice", "Bob", "Charlie"]]
+    )
+
+
+def test_len(all_players):
+    assert len(all_players) == 3
+
+
+def test_getitem(all_players):
+    expected_names = ["Alice", "Bob", "Charlie"]
+    for i in range(3):
+        assert all_players[i].name == expected_names[i]
+
+
+def test_iterator(all_players):
+    expected_names = ["Alice", "Bob", "Charlie"]
+    for actual, expect in zip(all_players, expected_names):
+        assert actual.name == expect

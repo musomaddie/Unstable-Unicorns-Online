1829bf0bd467e509938c1ff15a15cfcc625a9418
Maddie Wagner <musomaddy@gmail.com>
2023-10-10 09:24:03 +1100

    Added get_baby method to nursery.

--- game_details/nursery/nursery.py
+++ game_details/nursery/nursery.py
@@ -1,9 +1,14 @@
 from dataclasses import dataclass, field
 
-from game_details.card import Card, CardType, MultipleCardsHolder
+from game_details.card import Card, CardType, CardStack
 
 
 @dataclass
-class Nursery(MultipleCardsHolder):
-    babies: list[Card] = field(
+class Nursery(CardStack):
+    """ Managers the nursery. """
+    cards: list[Card] = field(
         default_factory=lambda: [Card("Baby Unicorn", CardType.BABY_UNICORN, "text") for _ in range(25)])
+
+    def get_baby(self):
+        """ Removes and returns the first baby from the nursery. """
+        return self.pop_top()

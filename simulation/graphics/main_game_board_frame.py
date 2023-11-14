ea3b31a1a48396ca8d741f5701651e860e75b492
Madeleine Wagner <musomaddy@gmail.com>
2023-11-15 10:15:51 +1100

    Added basic draw and discard piles to the center table space.

--- simulation/graphics/main_game_board_frame.py
+++ simulation/graphics/main_game_board_frame.py
@@ -9,10 +9,11 @@ class MainGameBoardFrame:
 
     def __init__(self, parent: ttk.Frame):
         self.root_frame = ttk.Frame(parent)
-        self.setup_nursery()
-
-        # Applied to parent object.
         self.root_frame.grid(column=1, row=1, sticky="NSEW")
+
+        self.setup_nursery()
+        self.setup_deck()
+        self.setup_discard_pile()
 
     def setup_nursery(self):
         """ setups the nursery. """
@@ -20,3 +21,17 @@ class MainGameBoardFrame:
         canvas = CardDeckCanvas(self.root_frame)
         lbl.grid(column=0, row=0, sticky="NSEW")
         canvas.root.grid(column=0, row=1, sticky="NSEW")
+
+    def setup_deck(self):
+        """ setups the deck (draw pile) """
+        lbl = ttk.Label(self.root_frame, text="Deck (draw pile)")
+        canvas = CardDeckCanvas(self.root_frame)
+        lbl.grid(column=1, row=0, sticky="NSEW")
+        canvas.root.grid(column=1, row=1, sticky="NSEW")
+
+    def setup_discard_pile(self):
+        """ setups the discard pile. """
+        lbl = ttk.Label(self.root_frame, text="Discard pile")
+        canvas = CardDeckCanvas(self.root_frame)
+        lbl.grid(column=2, row=0, sticky="NSEW")
+        canvas.root.grid(column=2, row=1, sticky="NSEW")

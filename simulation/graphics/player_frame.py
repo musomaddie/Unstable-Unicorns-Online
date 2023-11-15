ee3911f0463d5003617bd1ccac2aadf893027cab
Madeleine Wagner <musomaddy@gmail.com>
2023-11-15 12:50:19 +1100

    Added basic unicorns and stable layers to player mats. using a data class within the orientation enum to handle corresponding values sensibly.

--- simulation/graphics/player_frame.py
+++ simulation/graphics/player_frame.py
@@ -2,7 +2,6 @@
 from tkinter import ttk
 
 from simulation.graphics.card_deck_canvas import CardDeckCanvas
-from simulation.graphics.utility import GridPosition as gP
 from simulation.graphics.utility.orientation import Orientation
 
 
@@ -39,26 +38,40 @@ class PlayerFrame:
     | c7    | c7       | c7        |
     --------------------------------
     """
+    max_hand_size = 7
 
     def __init__(self, parent: ttk.Frame, player_name: str, orientation: Orientation):
         self.root = ttk.Frame(parent)
         name_lbl = ttk.Label(self.root, text=player_name)
         name_lbl.grid(column=0, row=0, sticky="NSEW")
         self._create_hand(orientation)
+        self._create_stable(orientation)
 
     def _create_hand(self, orientation: Orientation):
         """ creates the layout for the players hand."""
         hand_frame = ttk.Frame(self.root)
         hand_frame.grid(column=0, row=1, sticky="NSEW")
 
-        lbl = ttk.Label(hand_frame, text="Hand")
-        lbl.grid(column=0, row=0, sticky="NSEW")
+        lbl = ttk.Label(hand_frame, text="Hand", background="cyan")
+        lbl.grid(column=0, row=0, sticky="NSEW", columnspan=orientation.value.label_colspan)
 
-        max_hand_size = 7
-        card_spots = [CardDeckCanvas(hand_frame) for _ in range(max_hand_size)]
-        card_pos = [
-            gP(col=i, row=1) for i in range(max_hand_size)] if orientation == Orientation.HORIZONTAL else [
-            gP(col=0, row=i + 1) for i in range(max_hand_size)]
-
-        for canvas, pos in zip(card_spots, card_pos):
+        card_spots = [CardDeckCanvas(hand_frame) for _ in range(self.max_hand_size)]
+        for canvas, pos in zip(card_spots, orientation.value.hand_card_pos):
             canvas.root.grid(column=pos.col, row=pos.row, sticky="NSEW")
+
+    def _create_stable(self, orientation: Orientation):
+        """ Creates a stable layout. """
+        stable_frame = ttk.Frame(self.root)
+        stable_frame.grid(
+            column=orientation.value.stable_pos.col,
+            row=orientation.value.stable_pos.row,
+            sticky="NSEW",
+            columnspan=orientation.value.stable_colspan)
+
+        stable_lbl = ttk.Label(stable_frame, text="Stable", background="#3344ff")
+        stable_lbl.grid(column=0, row=0, sticky="NSEW", columnspan=orientation.value.stable_colspan)
+
+        unicorn_frame = ttk.Frame(stable_frame)
+        unicorn_frame.grid(column=0, row=1, sticky="NSEW")
+        unicorn_lbl = ttk.Label(unicorn_frame, text="Unicorns", background="#33ff22")
+        unicorn_lbl.grid(column=0, row=0, sticky="NSEW", columnspan=orientation.value.label_colspan)
--- simulation/graphics/utility/orientation.py
+++ simulation/graphics/utility/orientation.py
@@ -1,8 +1,33 @@
 """ orientation enum """
-from enum import Enum, auto
+from dataclasses import dataclass
+from enum import Enum
+
+from simulation.graphics.utility import GridPosition
+
+
+@dataclass
+class Values:
+    """ values relating to orientation """
+    label_colspan: int
+    stable_colspan: int
+    stable_pos: GridPosition
+    hand_card_pos: list[GridPosition]
 
 
 class Orientation(Enum):
     """ Represents which way a particular class is orientated. """
-    VERTICAL = auto()  # UP / DOWN axis in the long side.
-    HORIZONTAL = auto()  # LEFT / RIGHT axis is the  is long side
+    # UP / DOWN axis is the long side.
+    VERTICAL = Values(
+        label_colspan=1,
+        stable_colspan=2,
+        stable_pos=GridPosition(col=1, row=1),
+        hand_card_pos=[GridPosition(col=0, row=(i + 1)) for i in range(7)],
+    )
+
+    # LEFT / RIGHT axis is the  is long side
+    HORIZONTAL = Values(
+        label_colspan=7,
+        stable_colspan=7,
+        stable_pos=GridPosition(col=0, row=2),
+        hand_card_pos=[GridPosition(col=i, row=1) for i in range(7)],
+    )

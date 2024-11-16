78e9916fff290cf35ab6b8c38734bdc8e0fb6f62
Madeleine Wagner <musomaddy@gmail.com>
2024-11-17 09:47:19 +1100

    Added the basics for starting a game via CLI - i.e. gathering player names.

---
+++ unstable_unicorns_game/cli_runner/start_game.py
@@ -0,0 +1,12 @@
+from unstable_unicorns_game.game_details.game.factory import game_factory
+from unstable_unicorns_game.play_deciders import DeciderFactory, DeciderType
+
+
+def start_game(player_names: list[str]):
+    """
+    Starts a game of unstable unicorns to be run via the command line interface.
+    :param player_names: a list of the names of players for this game.
+    :return:
+    """
+
+    this_game = game_factory.create(player_names, DeciderFactory(DeciderType.CLI))

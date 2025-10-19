27662081087c21c0543d5123e29fe84fdc59b995
Madeleine Wagner <musomaddy@gmail.com>
2025-10-19 11:19:45 +1100

    Made filter iterable and improved tests.

--- tests/game_details/card/action/test_filter.py
+++ tests/game_details/card/action/test_filter.py
@@ -14,32 +14,32 @@ class TestCreate:
     @pytest.mark.parametrize(
         ("input_dict", "expected_filters"),
         [
-            ({"filters": ["upgrade"]}, {FilterType.UPGRADE}),
-            ({"filters": ["none"]}, {FilterType.NONE}),
-            ({"filters": ["upgrade", "none"]}, {FilterType.UPGRADE, FilterType.NONE}),
-            ({"filters": []}, set()),
+            ({"filters": ["upgrade"]}, [FilterType.UPGRADE]),
+            ({"filters": ["none"]}, [FilterType.NONE]),
+            ({"filters": ["upgrade", "none"]}, [FilterType.UPGRADE, FilterType.NONE]),
+            ({"filters": []}, []),
         ]
     )
     def test_create_with_valid_filters(self, input_dict, expected_filters):
         result = Filter.create(input_dict)
-        assert result.filters == expected_filters
+        assert set(result.filters) == set(expected_filters)
 
     def test_create_missing_filters_key(self):
         result = Filter.create({})
-        assert result.filters == set()
+        assert result.filters == []
 
     def test_create_with_invalid_filter_string(self):
         result = Filter.create({"filters": ["invalid_filter"]})
-        assert result.filters == {FilterType.NONE}
+        assert result.filters == [FilterType.NONE]
 
     def test_merges_identical_filters(self):
         result = Filter.create({"filters": ["none", "none"]})
-        assert result.filters == {FilterType.NONE}
+        assert result.filters == [FilterType.NONE]
 
     def test_create_with_mixed_valid_invalid_filters(self):
         result = Filter.create({"filters": ["upgrade", "invalid", "none"]})
-        assert result.filters == {FilterType.UPGRADE, FilterType.NONE}
+        assert set(result.filters) == {FilterType.UPGRADE, FilterType.NONE}
 
     def test_create_with_empty_string_filter(self):
         result = Filter.create({"filters": [""]})
-        assert result.filters == {FilterType.NONE}
+        assert result.filters == [FilterType.NONE]
--- unstable_unicorns_game/game_details/card/action/filter.py
+++ unstable_unicorns_game/game_details/card/action/filter.py
@@ -7,17 +7,36 @@ from unstable_unicorns_game.game_details.card.action.filter_type import FilterTy
 @dataclass
 class Filter:
     """ List of filters to be applied """
-    filters: set[FilterType]
+    filters: list[FilterType]
+
+    def __len__(self):
+        return len(self.filters)
+
+    def __iter__(self) -> '_FilterIterator':
+        return _FilterIterator(self)
 
     @classmethod
     def create_default(cls) -> 'Filter':
         """ Creates a filter with an empty list."""
-        return cls(filters=set())
+        return cls(filters=[])
 
     @classmethod
     def create(cls, action_dict: dict) -> 'Filter':
         """ Creates a filter object from the given dictionary. """
         if "filters" not in action_dict:
             return cls.create_default()
+        return cls(list(set([FilterType.create_from_string(f) for f in action_dict["filters"]])))
 
-        return cls(filters=set([FilterType.create_from_string(f) for f in action_dict["filters"]]))
+
+class _FilterIterator:
+    """ An iterator for the filter"""
+
+    def __init__(self, filter: Filter):
+        self._filter = filter
+        self._index = 0
+
+    def __next__(self) -> FilterType:
+        if self._index < len(self._filter):
+            self._index += 1
+            return self._filter.filters[self._index - 1]
+        raise StopIteration

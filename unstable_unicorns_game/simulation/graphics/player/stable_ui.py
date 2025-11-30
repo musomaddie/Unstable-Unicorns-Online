7f8fcc586aa81f3963b4eba5bd99fb7843c8c8ef
musomaddie <musomaddy@gmail.com>
2025-11-30 14:01:48 +1100

    Clean up stable card row somewhat.

--- unstable_unicorns_game/simulation/graphics/player/stable_ui.py
+++ unstable_unicorns_game/simulation/graphics/player/stable_ui.py
@@ -7,8 +7,7 @@ from unstable_unicorns_game.game.cards.card import Card
 from unstable_unicorns_game.game.cards.card_type import CardType
 from unstable_unicorns_game.game.cards.multiple_cards_holder import MultipleCardsHolder
 from unstable_unicorns_game.game.player.stable import Stable
-from unstable_unicorns_game.simulation.graphics.cards.card_ui import CardUi
-from unstable_unicorns_game.simulation.graphics.cards.cards_ui import CardToUi
+from unstable_unicorns_game.simulation.graphics.cards.cards_ui import CardsRow
 from unstable_unicorns_game.simulation.graphics.utility import colours, styles
 from unstable_unicorns_game.simulation.graphics.widgets.container_widget import ContainerWidget
 from unstable_unicorns_game.simulation.graphics.widgets.label import CenteredLabel, Label, RightAlignedLabel
@@ -67,61 +66,39 @@ class StableCardsPile(StableCardsContainer):
 
 
 class StableCardsRow(StableCardsContainer):
-    # TODO -> can be simplified by making a container for widget + card to ui for each of unicorns / upgrades /
-    #  downgrades.
-    unicorns_to_ui: list[CardToUi]
-    upgrades_to_ui: list[CardToUi]
-    downgrades_to_ui: list[CardToUi]
+    unicorns_row: CardsRow
+    upgrades_row: CardsRow
+    downgrades_row: CardsRow
 
-    unicorn_widget: ContainerWidget
-    upgrade_widget: ContainerWidget
-    downgrade_widget: ContainerWidget
+    # unicorns_to_ui: list[CardToUi]
+    # upgrades_to_ui: list[CardToUi]
+    # downgrades_to_ui: list[CardToUi]
+    #
+    # unicorn_widget: ContainerWidget
+    # upgrade_widget: ContainerWidget
+    # downgrade_widget: ContainerWidget
 
     def __init__(self, stable: Stable, **kwargs):
         super().__init__(stable, layout=QHBoxLayout(), **kwargs)
 
-        self.unicorns_to_ui = [CardToUi(card, CardUi(card)) for card in stable.unicorns]
-        self.upgrades_to_ui = [CardToUi(card, CardUi(card)) for card in stable.upgrades]
-        self.downgrades_to_ui = [CardToUi(card, CardUi(card)) for card in stable.downgrades]
+        self.unicorns_row = CardsRow(stable.unicorns)
+        self.upgrades_row = CardsRow(stable.upgrades)
+        self.downgrades_row = CardsRow(stable.downgrades)
 
-        self.unicorn_widget = ContainerWidget(QHBoxLayout(), style_identifier="cards-row")
-        self.upgrade_widget = ContainerWidget(QHBoxLayout(), style_identifier="cards-row")
-        self.downgrade_widget = ContainerWidget(QHBoxLayout(), style_identifier="cards-row")
+        # TODO -> is removing margins here defs correct?
+        self.unicorns_row.remove_margins()
+        self.upgrades_row.remove_margins()
+        self.downgrades_row.remove_margins()
 
-        self.unicorn_widget.add_widgets(*[ui.ui for ui in self.unicorns_to_ui])
-        self.upgrade_widget.add_widgets(*[ui.ui for ui in self.upgrades_to_ui])
-        self.downgrade_widget.add_widgets(*[ui.ui for ui in self.downgrades_to_ui])
-        self.unicorn_widget.remove_margins()
-        self.upgrade_widget.remove_margins()
-        self.downgrade_widget.remove_margins()
-
-        self.add_widgets(*[self.unicorn_widget, self.upgrade_widget, self.downgrade_widget])
-
-    def _add_missing_card_ui(
-            self, missing_ui_list: list[CardToUi], corresponding_list: MultipleCardsHolder,
-            changed_widget: ContainerWidget):
-        ui_ids_list = [card_ui.card_id() for card_ui in missing_ui_list]
-        for card in corresponding_list:
-            if card.unique_id in ui_ids_list:
-                continue
-            new_card_ui = CardUi(card)
-            changed_widget.append_widget(new_card_ui)
-            missing_ui_list.append(CardToUi(card, new_card_ui))
-
-            # TODO -> do we have to wipe the remaining ones too ??
+        self.add_widgets(
+            self.unicorns_row, self.upgrades_row, self.downgrades_row)
 
     def update(self):
-        if len(self.unicorns_to_ui) != self.unicorns:
-            if len(self.unicorns) > len(self.unicorns_to_ui):
-                self._add_missing_card_ui(self.unicorns_to_ui, self.unicorns, self.unicorn_widget)
+        self.unicorns_row.update()
+        self.upgrades_row.update()
+        self.downgrades_row.update()
 
-        if len(self.upgrades_to_ui) != self.upgrades:
-            if len(self.upgrades) > len(self.upgrades_to_ui):
-                self._add_missing_card_ui(self.upgrades_to_ui, self.upgrades, self.upgrade_widget)
-
-        if len(self.downgrades_to_ui) != self.downgrades:
-            if len(self.downgrades) > len(self.downgrades_to_ui):
-                self._add_missing_card_ui(self.downgrades_to_ui, self.downgrades, self.downgrade_widget)
+        # TODO -> I might need to update the parent holder as well.
 
 
 class StableContainerUi(ContainerWidget):
@@ -138,9 +115,7 @@ class StableContainerUi(ContainerWidget):
         self.remove_margins()
 
 
-def _create_expanded_view(stable: Stable):
-    # Creates something that lets us update the cards in the stable. We need to link the cards to their UIs in a
-    # sensible way.
+def _create_expanded_view(stable: Stable) -> StableContainerUi:
     cards_container = StableCardsRow(stable)
     label = RightAlignedLabel("Stable", style_identifier="lbl")
 
@@ -154,7 +129,7 @@ def _create_compact_view(stable: Stable) -> ContainerWidget:
     return StableCardsPile(stable)
 
 
-def _create_turn_view(stable: Stable) -> ContainerWidget:
+def _create_turn_view(stable: Stable) -> StableContainerUi:
     cards_container = StableCardsRow(stable)
     cards_container.horizontal_stretch(1)
     label = CenteredLabel("Stable", style_identifier="cards-label")
@@ -167,9 +142,9 @@ def _create_turn_view(stable: Stable) -> ContainerWidget:
 class StableUi:
     stable: Stable
 
-    expanded_view: ContainerWidget
+    expanded_view: StableContainerUi
     compact_view: ContainerWidget
-    turn_view: ContainerWidget
+    turn_view: StableContainerUi
 
     def __init__(self, stable: Stable):
         self.stable = stable
